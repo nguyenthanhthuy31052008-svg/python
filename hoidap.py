6083b0810b8f4b151abb7b1f962c87c9fea5012b
nguyenthanhthuy31052008-svg <nguyenthanhthuy31052008@gmail.com>
2026-02-11 15:36:41 +0700

    Add files via upload

---
+++ hoidap.py
@@ -0,0 +1,9 @@
+print("python có đúng trong an ninh mạng không?")
+tl=input("đúng/sai")
+
+if tl == "đúng":
+   print("chính xác!Python dùng rất nhiều trong báo cáo")
+elif tl == "sai":
+   print("Sai rồi ,Python dùng rất nhiều trong security.")
+else:
+   print("Vui lòng chỉ nhập đúng hoặc sai")
